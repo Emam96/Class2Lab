725388f69bd239cb08abe2c40d5354672898d379
Emam96 <emamsh990@gmail.com>
2021-06-21 16:29:11 +0300

    fixed some stuff

--- JS/script.js
+++ JS/script.js
@@ -12,94 +12,92 @@ if (userName == "") {
 }
 
 function quizFunction() {
-  let age = prompt("Do you want to know how old I am?").toLowerCase();
+  let age = prompt("Do you think Iam 24?").toLowerCase();
   while (age != "yes" && age != "no" && age != "y" && age != "n") {
     age = prompt("please pick: yes, no");
   }
 
   if (age == "yes") {
-    document.getElementById("age").innerHTML =
-      "Iam 24 years old, Thank you for asking!";
+    document.getElementById("age").innerHTML = "Do you think Iam 24? you answered by "+age+" You was right, Iam 24 years old, Great!";
   }
   if (age == "no") {
     document.getElementById("age").innerHTML =
-      "You didn't want to know my age :c";
+    "Do you think Iam 24? you answered by "+age+" You was wrong, Iam 24.";
   }
   if (age == "y") {
     document.getElementById("age").innerHTML =
-      "Iam 24 years old, Thank you for asking!";
+    "Do you think Iam 24? you answered by "+age+" You was right, Iam 24 years old, Great!";
   }
   if (age == "n") {
     document.getElementById("age").innerHTML =
-      "You didn't want to know my age :c";
+    "Do you think Iam 24? you answered by "+age+" You was wrong, Iam 24.";
   }
 
-  let edu = prompt("Do you want to know about my education?").toLowerCase();
+  let edu = prompt("Did I graduated from al alBayt university ?").toLowerCase();
   while (edu != "yes" && edu != "no" && edu != "y" && edu != "n") {
     edu = prompt("please pick: yes, no");
   }
 
   if (edu == "yes") {
-    document.getElementById("edu").innerHTML =
-      "I studied Business at Yarmouk university, I graduated with a very good score.";
+    document.getElementById("edu").innerHTML = "Did I graduated from al alBayt university ?, you answered by "+edu+" :No. I graduated from Yarmouk university actually";
   }
   if (edu == "no") {
     document.getElementById("edu").innerHTML =
-      "You didn't want to know my education either :c";
+    "Did I graduated from al alBayt university ?, you answered by "+edu+" :Yes, I studied Business at Yarmouk university, I graduated with a very good score.";
   }
   if (edu == "y") {
     document.getElementById("edu").innerHTML =
-      "I studied Business at Yarmouk university, I graduated with a very good score.";
+    "Did I graduated from al alBayt university ?, you answered by "+edu+" :No. I graduated from Yarmouk university actually";
   }
   if (edu == "n") {
     document.getElementById("edu").innerHTML =
-      "You didn't want to know my education either :c";
+    "Did I graduated from al alBayt university ?, you answered by "+edu+" :Yes, I studied Business at Yarmouk university, I graduated with a very good score.";
   }
 
 
-  let xp = prompt("Do you want to know about my work experience?").toLowerCase();
+  let xp = prompt("Did I worked for Umniah ?").toLowerCase();
   while (xp != "yes" && xp != "no" && xp != "y" && xp != "n") {
     xp = prompt("please pick: yes, no");
   }
 
   if (xp == "yes") {
     document.getElementById("xp").innerHTML =
-      "I worked as a customer care agent at Extensya first, then I moved to Umniah where I was a shop sales person, and now I am here.";
+    "Did I worked for Umniah ? you answered by "+xp+" Yes, I worked as a customer care agent at Extensya first, then I moved to Umniah where I was a shop sales person, and now I am here.";
   }
   if (xp == "no") {
     document.getElementById("xp").innerHTML =
-      "You didn't want to know my work experience, nevermind, kinda boring";
+    "Did I worked for Umniah ? you answered by "+xp+" No I actually did.";
   }
   if (xp == "y") {
     document.getElementById("xp").innerHTML =
-      "I worked as a customer care agent at Extensya first, then I moved to Umniah where I was a shop sales person, and now I am here.";
+    "Did I worked for Umniah ? you answered by "+xp+" Yes, I worked as a customer care agent at Extensya first, then I moved to Umniah where I was a shop sales person, and now I am here.";
   }
   if (xp == "n") {
     document.getElementById("xp").innerHTML =
-      "You didn't want to know my work experience, nevermind, kinda boring";
+    "Did I worked for Umniah ? you answered by "+xp+" No I actually did.";
   }
 
 
-  let city = prompt("Do you want to know from where I am?").toLowerCase();
+  let city = prompt("Do you think I live in Amman?").toLowerCase();
   while (city != "yes" && city != "no" && city != "y" && city != "n") {
     city = prompt("please pick: yes, no");
   }
 
   if (city == "yes") {
     document.getElementById("city").innerHTML =
-      "Iam from Jordan, and I live in Irbid";
+    "Do you think I live in Amman? you answered by "+city+" Nope, I live in Irbid";
   }
   if (city == "no") {
     document.getElementById("city").innerHTML =
-      "You didn't want to know my location, thats fair";
+    "Do you think I live in Amman? you answered by "+city+" Right!. I live in Irbid";
   }
   if (city == "y") {
     document.getElementById("city").innerHTML =
-      "Iam from Jordan, and I live in Irbid";
+    "Do you think I live in Amman? you answered by "+city+" Nope, I live in Irbid";
   }
   if (city == "n") {
     document.getElementById("city").innerHTML =
-      "You didn't want to know my location, thats fair";
+    "Do you think I live in Amman? you answered by "+city+" Right!. I live in Irbid";
   }
 
 
